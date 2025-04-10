c15f9af1a762d64c501fea710ad7d45a4e476ee6
Devil666coin <theunholycoin@gmail.com>
2025-04-10 09:51:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+document.getElementById('connectWallet').onclick = function() {
+    document.getElementById('walletOptions').style.display = 'block';
+};
+function connectMetaMask() {
+    alert("Connecting to MetaMask...");
+}
+function connectTrustWallet() {
+    alert("Connecting to Trust Wallet...");
+}
+function connectWalletConnect() {
+    alert("Connecting to WalletConnect...");
+}
